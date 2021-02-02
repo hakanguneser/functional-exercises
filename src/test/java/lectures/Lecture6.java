e4f5024bc08d08a5d9db499963c72ef033809072
hakanguneser <hakan.guneser@gmail.com>
2021-02-03 00:07:46 +0300

    Update Lecture6.java

--- src/test/java/lectures/Lecture6.java
+++ src/test/java/lectures/Lecture6.java
@@ -12,14 +12,17 @@ public class Lecture6 {
 
   @Test
   public void findAny() throws Exception {
+    //koşulu sağlayan herhangi değeri getirir. paralel çalışarak daha performanslı arama yapabilir find first en baştan başlayıp tek tek kontrol eder.
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
+    Integer i = Arrays.stream(numbers).filter(numbersLessThan10).findAny().get();
+    System.out.println(i);
   }
 
   @Test
   public void findFirst() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
+    Integer i = Arrays.stream(numbers).filter(numbersLessThan10).findFirst().get();
+    System.out.println(i);
   }
 }
 
