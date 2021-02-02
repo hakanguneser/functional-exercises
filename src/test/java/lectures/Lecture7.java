a3a4b13b180039c978e6eab8e96b493d490a30bb
hakanguneser <hakan.guneser@gmail.com>
2021-02-03 00:24:30 +0300

    Update Lecture7.java

--- src/test/java/lectures/Lecture7.java
+++ src/test/java/lectures/Lecture7.java
@@ -5,61 +5,89 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import beans.Car;
 import com.google.common.collect.ImmutableList;
+
 import java.math.BigDecimal;
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.stream.Collectors;
+
 import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture7 {
+    private static final String VW = "Volkswagen";
 
-  @Test
-  public void count() throws Exception {
+    @Test
+    public void count() throws Exception {
+        ImmutableList<Car> cars = MockData.getCars();
+        Long result = cars.stream()
+                .filter(car -> car.getMake().equals(VW))
+                .count();
+        System.out.println(result);
+    }
 
-  }
+    @Test
+    public void min() throws Exception {
+        ImmutableList<Car> cars = MockData.getCars();
+        Double result = cars.stream()
+                .filter(car -> car.getMake().equals(VW))
+                .mapToDouble(Car::getPrice)
+                .min()
+                .getAsDouble();
 
-  @Test
-  public void min() throws Exception {
+        System.out.println(result);
+    }
 
-  }
+    @Test
+    public void max() throws Exception {
+        ImmutableList<Car> cars = MockData.getCars();
+        Double result = cars.stream()
+                .filter(car -> car.getMake().equals(VW))
+                .mapToDouble(Car::getPrice)
+                .max()
+                .getAsDouble();
 
-  @Test
-  public void max() throws Exception {
-
-  }
+        System.out.println(result);
+    }
 
 
-  @Test
-  public void average() throws Exception {
-    List<Car> cars = MockData.getCars();
+    @Test
+    public void average() throws Exception {
+        List<Car> cars = MockData.getCars();
+        
+        Double result = cars.stream()
+                .filter(car -> car.getMake().equals(VW))
+                .mapToDouble(Car::getPrice)
+                .average()
+                .getAsDouble();
 
-  }
+        System.out.println(result);
+    }
 
-  @Test
-  public void sum() throws Exception {
-    List<Car> cars = MockData.getCars();
-    double sum = cars.stream()
-        .mapToDouble(Car::getPrice)
-        .sum();
-    BigDecimal bigDecimalSum = BigDecimal.valueOf(sum);
-    System.out.println(sum);
-    System.out.println(bigDecimalSum);
+    @Test
+    public void sum() throws Exception {
+        List<Car> cars = MockData.getCars();
+        double sum = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .sum();
+        BigDecimal bigDecimalSum = BigDecimal.valueOf(sum);
+        System.out.println(sum);
+        System.out.println(bigDecimalSum);
 
-  }
+    }
 
-  @Test
-  public void statistics() throws Exception {
-    List<Car> cars = MockData.getCars();
-    DoubleSummaryStatistics statistics = cars.stream()
-        .mapToDouble(Car::getPrice)
-        .summaryStatistics();
-    System.out.println(statistics);
-    System.out.println(statistics.getAverage());
-    System.out.println(statistics.getCount());
-    System.out.println(statistics.getMax());
-    System.out.println(statistics.getMin());
-    System.out.println(statistics.getSum());
-  }
+    @Test
+    public void statistics() throws Exception {
+        List<Car> cars = MockData.getCars();
+        DoubleSummaryStatistics statistics = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .summaryStatistics();
+        System.out.println(statistics);
+        System.out.println(statistics.getAverage());
+        System.out.println(statistics.getCount());
+        System.out.println(statistics.getMax());
+        System.out.println(statistics.getMin());
+        System.out.println(statistics.getSum());
+    }
 
 }
