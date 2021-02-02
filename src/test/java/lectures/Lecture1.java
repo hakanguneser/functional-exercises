f7b08435cf0ad61dd7b9719373fd00375ccf0bdd
hakanguneser <hakan.guneser@gmail.com>
2021-02-02 22:50:25 +0300

    Lecture 1 done

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import beans.Person;
 
 import com.google.common.collect.ImmutableList;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,17 +17,27 @@ import org.junit.Test;
 
 public class Lecture1 {
 
-  @Test
-  public void imperativeApproach() throws IOException {
-    List<Person> people = MockData.getPeople();
-    // 1. Find people aged less or equal 18
-    // 2. Then change implementation to find first 10 people
+    @Test
+    public void imperativeApproach() throws IOException {
+        List<Person> people = MockData.getPeople();
+        int count = 1;
+        for (Person personality : people) {
+            if (personality.getAge() <= 18) {
+                System.out.println(personality.toString());
+                count++;
+            }
+            if (count == 11) {
+                break;
+            }
+        }
+    }
 
-  }
-
-  @Test
-  public void declarativeApproachUsingStreams() throws Exception {
-    ImmutableList<Person> people = MockData.getPeople();
-
-  }
+    @Test
+    public void declarativeApproachUsingStreams() throws Exception {
+        MockData.getPeople().stream()
+                .filter(person -> person.getAge() <= 18)
+                .limit(10)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
 }
