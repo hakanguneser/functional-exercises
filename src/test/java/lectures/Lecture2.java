450e4982f03140c742b5b2f60246931339b6dc4c
hakanguneser <hakan.guneser@gmail.com>
2021-02-02 23:04:22 +0300

    Update Lecture2.java

--- src/test/java/lectures/Lecture2.java
+++ src/test/java/lectures/Lecture2.java
@@ -10,13 +10,21 @@ public class Lecture2 {
 
   @Test
   public void range() throws Exception {
-
+    System.out.println("for i");
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i);
+    }
+    System.out.println("range");
+    IntStream.range(0,10).forEach(System.out::println);
+    System.out.println("rangeClosed");
+    IntStream.rangeClosed(0,10).forEach(System.out::println);
   }
 
   @Test
   public void rangeIteratingLists() throws Exception {
     List<Person> people = MockData.getPeople();
-
+    IntStream.range(0, people.size())
+            .forEach(value -> System.out.println(people.get(value).toString()));
   }
 
   @Test
