091fbdf25b7373e30b8c7930d80b18af8f799b31
hakanguneser <hakan.guneser@gmail.com>
2021-02-02 23:18:30 +0300

    Update Lecture3.java

--- src/test/java/lectures/Lecture3.java
+++ src/test/java/lectures/Lecture3.java
@@ -12,12 +12,16 @@ public class Lecture3 {
   @Test
   public void min() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
-
+    Integer min = numbers.stream().min(Comparator.naturalOrder()).get();
+    System.out.println(min);
+    assertThat(min).isEqualTo(1);
   }
 
   @Test
   public void max() throws Exception {
     final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
-
+    Integer max = numbers.stream().max(Comparator.naturalOrder()).get();
+    System.out.println(max);
+    assertThat(max).isEqualTo(100);
   }
 }
