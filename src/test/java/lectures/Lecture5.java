a58f871c31b9c991c571cad03dd73d5c5193f675
hakanguneser <hakan.guneser@gmail.com>
2021-02-03 00:04:47 +0300

    Update Lecture5.java

--- src/test/java/lectures/Lecture5.java
+++ src/test/java/lectures/Lecture5.java
@@ -7,37 +7,49 @@ import beans.Car;
 import beans.Person;
 import beans.PersonDTO;
 import com.google.common.collect.ImmutableList;
+
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+
 import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture5 {
 
-  @Test
-  public void understandingFilter() throws Exception {
-    ImmutableList<Car> cars = MockData.getCars();
+    @Test
+    public void understandingFilter() throws Exception {
+        final String vw = "Volkswagen";
+        MockData.getCars().stream()
+                .filter(car -> car.getMake().equals(vw))
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
 
-  }
+    }
 
-  @Test
-  public void ourFirstMapping() throws Exception {
-    // transform from one data type to another
-    List<Person> people = MockData.getPeople();
+    @Test
+    public void ourFirstMapping() throws Exception {
+        // transform from one data type to another
+        MockData.getPeople().stream()
+                .map(PersonDTO::map)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
 
-  }
+    @Test
+    public void averageCarPrice() throws Exception {
+        // calculate average of car prices
+        ImmutableList<Car> cars = MockData.getCars();
+        Double d = cars.stream()
+                .mapToDouble(Car::getPrice)
+                .average().getAsDouble();
+        System.out.println(d);
+    }
 
-  @Test
-  public void averageCarPrice() throws Exception {
-    // calculate average of car prices
+    @Test
+    public void test() throws Exception {
 
-  }
-
-  @Test
-  public void test() throws Exception {
-
-  }
+    }
 }
 
 
