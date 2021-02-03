697359105fd34f0292a055f9f18e93ad7fce83fd
hakanguneser <hakan.guneser@gmail.com>
2021-02-03 22:42:53 +0300

    Update Lecture11.java

--- src/test/java/lectures/Lecture11.java
+++ src/test/java/lectures/Lecture11.java
@@ -1,22 +1,30 @@
 package lectures;
 
 import com.google.common.collect.ImmutableList;
+
 import java.util.List;
 import java.util.stream.Collectors;
+
 import mockdata.MockData;
 import org.junit.Test;
 
 public class Lecture11 {
 
-  @Test
-  public void joiningStrings() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+    @Test
+    public void joiningStrings() throws Exception {
+        List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+        String join = "";
+        for (String s : names) {
+            join += s + ", ";
+        }
+        System.out.println(join.substring(0, join.length() - 2));
+    }
 
-  }
+    @Test
+    public void joiningStringsWithStream() throws Exception {
+        List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
+        String result = names.stream().collect(Collectors.joining(", "));
 
-  @Test
-  public void joiningStringsWithStream() throws Exception {
-    List<String> names = ImmutableList.of("anna", "john", "marcos", "helena", "yasmin");
-
-  }
+        System.out.println(result);
+    }
 }
